d972d4c076e6eeb3fcc39107f1bcc27a0562033a
Mikhail <mishajudoist@gmail.com>
2022-08-02 15:55:18 +0900

    added linked list

--- chapter3/linkedList.go
+++ chapter3/linkedList.go
@@ -21,7 +21,59 @@ func (linkedList *LinkedList) AddToHead(property int) {
 	linkedList.headNode = &node
 }
 
-//IterateList
+// IterateList method
+func (linkedList *LinkedList) IterateList() {
+	for node := linkedList.headNode; node != nil; node = node.nextNode {
+		fmt.Println(node.property)
+	}
+}
+
+// LastNode method
+func (linkedList *LinkedList) LastNode() *Node {
+	var lastNode *Node
+	for node := linkedList.headNode; node != nil; node = node.nextNode {
+		if node.nextNode == nil {
+			lastNode = node
+		}
+	}
+	return lastNode
+}
+
+// AddToEnd method
+func (linkedList *LinkedList) AddToEnd(property int) {
+	var node = &Node{}
+	node.property = property
+	node.nextNode = nil
+	lastNode := linkedList.LastNode()
+	if lastNode != nil {
+		lastNode.nextNode = node
+	}
+}
+
+// NodeWithValue method
+func (linkedList *LinkedList) NodeWithValue(property int) *Node {
+	var nodeWith *Node
+	for node := linkedList.headNode; node != nil; node = node.nextNode {
+		if node.property == property {
+			nodeWith = node
+			break
+		}
+	}
+	return nodeWith
+}
+
+// AddAfter method
+func (linkedList *LinkedList) AddAfter(nodeProperty, property int) {
+	var node = &Node{}
+	node.property = property
+	node.nextNode = nil
+
+	nodeWith := linkedList.NodeWithValue(nodeProperty)
+	if nodeWith != nil {
+		node.nextNode = nodeWith.nextNode
+		nodeWith.nextNode = node
+	}
+}
 
 func main() {
 	linkedList := LinkedList{}
@@ -29,5 +81,11 @@ func main() {
 	linkedList.AddToHead(3)
 	linkedList.AddToHead(5)
 	fmt.Println(linkedList.headNode.property)
-
+	linkedList.IterateList()
+	fmt.Println(linkedList.LastNode().property)
+	linkedList.AddToEnd(12)
+	linkedList.IterateList()
+	fmt.Println(linkedList.NodeWithValue(12))
+	linkedList.AddAfter(1, 15)
+	linkedList.IterateList()
 }
