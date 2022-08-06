bbe381c30d51e2e14f600738b8a26a6ecd558760
Mikhail <mishajudoist@gmail.com>
2022-08-06 12:32:24 +0900

    done linkedList part 2

--- JunminLeeCourse/DataStructures/linkedLists/linkedList.go
+++ JunminLeeCourse/DataStructures/linkedLists/linkedList.go
@@ -1,5 +1,7 @@
 package linkedLists
 
+import "fmt"
+
 type Node struct {
 	Data int
 	next *Node
@@ -16,4 +18,37 @@ func (l *LinkedList) Prepend(n *Node) {
 
 	l.head.next = second
 	l.length++
+}
+
+func (l LinkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.Data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Println()
+}
+
+func (l *LinkedList) Delete(i int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.Data == i {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	previousToDelete := l.head
+	for previousToDelete.next.Data != i {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = l.head.next
+	}
+	// if next node data == i -> skip one node
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
 }
