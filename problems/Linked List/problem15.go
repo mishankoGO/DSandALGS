e8db1894439c26014f41b06e91a7597c71b498d1
Mikhail <mishajudoist@gmail.com>
2022-08-15 23:26:14 +0900

    done adding element into sorted linked list

---
+++ problems/Linked List/problem15.go
@@ -0,0 +1,29 @@
+package main
+
+import "DSandALGS/chapter3/ll"
+
+func InsertIntoSortedList(linkedList *ll.LinkedList, i int) {
+	for node := linkedList.HeadNode; node.NextNode != nil; node = node.NextNode {
+		if linkedList.HeadNode.Property >= i {
+			linkedList.AddToHead(i)
+			return
+		}
+		if i <= node.NextNode.Property {
+			linkedList.AddAfter(node.Property, i)
+			return
+		}
+	}
+	linkedList.AddToEnd(i)
+}
+
+func main() {
+	sortedLinkedList := ll.LinkedList{}
+	sortedLinkedList.AddToHead(1)
+	sortedLinkedList.AddToEnd(2)
+	sortedLinkedList.AddToEnd(4)
+	sortedLinkedList.AddToEnd(5)
+
+	InsertIntoSortedList(&sortedLinkedList, 0)
+
+	sortedLinkedList.IterateList()
+}
