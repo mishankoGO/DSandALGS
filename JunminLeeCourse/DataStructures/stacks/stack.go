6af145e9632af4a6f8e34dd58f632d852cbf2580
Mikhail <mishajudoist@gmail.com>
2022-08-21 16:16:26 +0900

    solved 17 using stacks

--- JunminLeeCourse/DataStructures/stacks/stack.go
+++ JunminLeeCourse/DataStructures/stacks/stack.go
@@ -2,18 +2,25 @@ package stacks
 
 // Stack class
 type Stack struct {
-	items []int
+	Items []int
 }
 
 // Push method
 func (s *Stack) Push(i int) {
-	s.items = append(s.items, i)
+	s.Items = append(s.Items, i)
 }
 
 // Pop method
 func (s *Stack) Pop() int {
-	l := len(s.items) - 1
-	item := s.items[l]
-	s.items = s.items[:l]
+	l := len(s.Items) - 1
+	item := s.Items[l]
+	s.Items = s.Items[:l]
+	return item
+}
+
+// Peek method
+func (s *Stack) Peek() int {
+	l := len(s.Items) - 1
+	item := s.Items[l]
 	return item
 }
---
+++ problems/Linked List/problem20.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"DSandALGS/chapter3/ll"
+	"fmt"
+)
+
+/* Give an algorithm for finding the merging point (Stacks) */
+// Time: O(n), Space: O(min(n, m))
+
+// Stack class
+type StackNode struct {
+	Items []*ll.Node
+}
+
+// Push method
+func (s *StackNode) Push(i *ll.Node) {
+	s.Items = append(s.Items, i)
+}
+
+// Pop method
+func (s *StackNode) Pop() *ll.Node {
+	l := len(s.Items) - 1
+	item := s.Items[l]
+	s.Items = s.Items[:l]
+	return item
+}
+
+// Peek method
+func (s *StackNode) Peek() *ll.Node {
+	l := len(s.Items) - 1
+	item := s.Items[l]
+	return item
+}
+
+func mergingPointStack(l1, l2 ll.LinkedList) (point *ll.Node) {
+	s1 := StackNode{}
+	s2 := StackNode{}
+	for node := l1.HeadNode; node != nil; node = node.NextNode {
+		s1.Push(node)
+	}
+	for node := l2.HeadNode; node != nil; node = node.NextNode {
+		s2.Push(node)
+	}
+
+	for len(s1.Items) > 0 {
+		elem1, elem2 := s1.Peek(), s2.Peek()
+		if elem1 == elem2 {
+			point = s1.Pop()
+			s2.Pop()
+		} else {
+			return point
+		}
+	}
+	return
+}
+
+func main() {
+	linkedList1 := ll.LinkedList{}
+	linkedList1.AddToHead(1)
+	linkedList1.AddToEnd(2)
+	linkedList1.AddToEnd(3)
+	linkedList1.AddToEnd(4)
+	linkedList1.AddToEnd(5)
+	linkedList1.AddToEnd(6)
+	linkedList1.AddToEnd(7)
+
+	linkedList2 := ll.LinkedList{}
+	linkedList2.AddToHead(1)
+	linkedList2.AddToEnd(2)
+	//linkedList2.LastNode().NextNode = linkedList1.NodeWithValue()
+
+	linkedList1.IterateList()
+	fmt.Println("next list")
+	linkedList2.IterateList()
+
+	fmt.Println(mergingPointStack(linkedList1, linkedList2))
+}
