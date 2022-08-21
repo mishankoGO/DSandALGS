c99a98260eec49e252b943bc205a1f7afc2ce73a
Mikhail <mishajudoist@gmail.com>
2022-08-21 12:39:19 +0900

    done problem16

---
+++ problems/Linked List/problem16.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"DSandALGS/chapter3/ll"
+	"fmt"
+)
+
+/* Reverse a singly linked list. */
+// Time: O(n), Space: O(1)
+
+func reverseLinkedList(linkedList *ll.LinkedList) {
+	var prev, next *ll.Node
+
+	for curr := linkedList.HeadNode; curr != nil; {
+		next = curr.NextNode // save next node
+		curr.NextNode = prev // point to the previous node
+		prev = curr          // point previous to it's new next
+		curr = next          // curr = curr.NextNode
+	}
+	linkedList.HeadNode = prev
+
+}
+
+func main() {
+	linkedList := ll.LinkedList{}
+	linkedList.AddToHead(4)
+	linkedList.AddToEnd(3)
+	linkedList.AddToEnd(2)
+	linkedList.AddToEnd(1)
+	linkedList.IterateList()
+	fmt.Print("\n")
+	reverseLinkedList(&linkedList)
+	linkedList.IterateList()
+}
